e5327e2db927a53c0a2dfd65216af323f8599107
reeedstudio <luweicong@qq.com>
2014-02-20 17:05:19 +0800

    write code

--- libraries/iRemote_hal/examples/iRemote_test/iRemote_test.ino
+++ libraries/iRemote_hal/examples/iRemote_test/iRemote_test.ino
@@ -6,6 +6,7 @@
 
 #define __Debug         0
 
+char ctrlKey = KEY_LEFT_GUI;
 
 unsigned char dtaRecv[20];              // receive data here
 
@@ -17,34 +18,57 @@ void setup()
     Serial.begin(115200);
     while(!Serial);
 #endif
-    
+
     iRemote.begin();
 
 }
 
 
+void write_code()
+{
+    delay(10);
+
+    // Type out "blink":
+    Keyboard.print("// iRemote demo, by loovee\r\n");
+    
+    Keyboard.print("void setup() \r\n{\r\n");
+    Keyboard.print("pinMode(13, OUTPUT);\r\n");
+    Keyboard.print("}\r\n");
+    Keyboard.println();
+    Keyboard.println();
+    Keyboard.print("void loop() \r\n{\r\n");
+    Keyboard.print("digitalWrite(13, HIGH);\r\n");
+    Keyboard.print("delay(1000);\r\n");
+
+    Keyboard.print("digitalWrite(13, LOW);\r\n");
+    Keyboard.print("delay(1000);\r\n");
+    Keyboard.print("}\r\n");
+
+    Keyboard.releaseAll();
+}
+
 void dataProc(int keyVal)
 {
 
     iRemote.redLedOn(0);
-    
+
     switch(keyVal)
     {
-    
+
         case 253:                       // left
-        
+
         iRemote.keyPressRelease(KEY_LEFT_ARROW);
         break;
-            
-        case 51:                        // right
-        
+
+        case 61:                        // right
+
         iRemote.keyPressRelease(KEY_RIGHT_ARROW);
         break;
-        
-        case 93:                                // logout
-        
 
-            
+        case 93:                                // logout
+
+
+
         Keyboard.press(KEY_LEFT_CTRL);
         Keyboard.press(KEY_LEFT_ALT);
         Keyboard.press(KEY_DELETE);
@@ -53,31 +77,37 @@ void dataProc(int keyVal)
         delay(100);
         Keyboard.press(KEY_LEFT_ALT);
         Keyboard.press('k');
-            
+
         delay(100);
         Keyboard.releaseAll();
-            
+
 
         break;
-        
-        
+
+
         case 31:                                // enter key
-        
+
         iRemote.keyPressRelease(KEY_RETURN);
-            
+
         delay(500);
-            
+
         iRemote.enterString("123456");
-            
+
         iRemote.keyPressRelease(KEY_RETURN);
-        
+
         break;
-        
-        
+
+
+        case 173:
+
+        write_code();
+
+        break;
+
         default:
-        
+
         ;
-    
+
     }
 }
 void loop()
@@ -85,9 +115,9 @@ void loop()
     if(iRemote.isGetIrDta())
     {
         int dta_last = iRemote.getIrLastDta();
-        
+
         Serial.println(dta_last);
-        
+
         if(dta_last>=0)
         {
             dataProc(dta_last);
